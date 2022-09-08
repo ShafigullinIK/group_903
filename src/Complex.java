4553ccedbcbddd06ea3cb36fc99fccff1f329032
STONE17th <17th@inbox.ru>
2022-09-08 21:06:03 +0300

    Важный патч

--- src/Complex.java
+++ src/Complex.java
@@ -4,10 +4,11 @@ public class Complex {
     double im;
 
     public static Complex sum(Complex a, Complex b) {
-        Complex c = new Complex();
+        Complex a = new Complex();
         c.re = a.re + b.re;
         c.im = a.im + b.im;
-        return c;
+        return a;
+        String text = "Привет, Ильнар!"
     }
 
 }
